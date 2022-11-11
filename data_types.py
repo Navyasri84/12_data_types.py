55e3577b3a092ee9dc93417f4004390c05add1d9
Navyasri84 <116783041+Navyasri84@users.noreply.github.com>
2022-11-11 17:35:25 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,30 @@
+# print(type(0))
+# print(type(4))
+# print(type(45664))
+# print(type(-5646))
+
+# print(type(0.3))
+# print(type(.5))
+# print(type(7.8))
+# print(type(-54.72))
+
+# print(type(''))
+# print(type(""))
+# print(type(''''''))
+# print(type(""""""))
+
+# print('hey'   ,   'hi')
+# print('hey '   ,   'hi')
+# print(''   ,   'hi')
+# print(' '   ,   'hi')
+
+# print('a'+'b')
+# print('a+b')
+# print("a+b")
+# print('''a+b''')
+
+print(5>7)
+print(5<7)
+
+print(type(True))
+print(type(False))
